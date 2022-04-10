7800ec4a6a291f4dede606d73d669030466fdaf3
Imad Machi <machii.imad@gmail.com>
2022-04-10 13:54:29 +0000

    add images

---
+++ src/statics/dummy-products.ts
@@ -0,0 +1,38 @@
+export const dummyProducts = [
+  {
+    name: "Fancy chair",
+    image: "./images/img.1",
+    brand: "Fako",
+    category: "Accessoires",
+    description: "This is a very fancy product from Fako brand",
+    price: 20.99,
+    countInStock: 21,
+  },
+  {
+    name: "BackPack",
+    image: "./images/img.2",
+    brand: "Fako",
+    category: "Accessoires",
+    description: "This is a very fancy product from Fako brand",
+    price: 18.99,
+    countInStock: 15,
+  },
+  {
+    name: "shawl",
+    image: "./images/img.3",
+    brand: "Fako",
+    category: "Accessoires",
+    description: "This is a very fancy product from Fako brand",
+    price: 5.99,
+    countInStock: 4,
+  },
+  {
+    name: "clock",
+    image: "./images/img.4",
+    brand: "Fako",
+    category: "Accessoires",
+    description: "This is a very fancy product from Fako brand",
+    price: 10.99,
+    countInStock: 10,
+  },
+];
