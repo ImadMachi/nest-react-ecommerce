3a26bacda567ae9bf5025088f2fcf4eb73aea1e1
SKITINA <skitinanouhaila44@gmail.com>
2022-04-10 13:41:23 +0000

    zadt log o zad cart

--- src/components/Navbars/Navbars.tsx
+++ src/components/Navbars/Navbars.tsx
@@ -1,22 +1,33 @@
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const Navbars = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Brand</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown title="categories" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">clothes</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Accesoires</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">chaussures</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Robes</NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#home">about</Nav.Link>
+            <Nav.Link href="#link">contact</Nav.Link>
+            </Nav>
+         <Nav>
+          <NavDropdown title={<FontAwesomeIcon icon={faUser}/>} id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Profil</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">log out</NavDropdown.Item>
+            
+            </NavDropdown>
+            <Nav.Link href="#cart">{<FontAwesomeIcon icon={faCartShopping}/>}</Nav.Link>
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/components/PromosProductCarousel/PromoProductCarousel.tsx
@@ -0,0 +1,9 @@
+
+
+const PromoProductCarousel = () => {
+  return (
+    <div>PromoProductCarousel</div>
+  )
+}
+
+export default PromoProductCarousel
